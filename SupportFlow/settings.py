a0945c7ee7487a95949c8ac0f5d7cce8382477d7
Михаил <mnovikov920@gmail.com>
2026-08-26 14:42:11 +0300

    Add custom user and PostgreSQL

--- SupportFlow/settings.py
+++ SupportFlow/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ib2d%@cr+8#b95f((-@#y1@1tguyh_d&)rdtx76n0)ctr9ctb'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,10 +79,15 @@ WSGI_APPLICATION = 'SupportFlow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env('DB_NAME'),
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWORD'),
+        "HOST": env('DB_HOST'),
+        "PORT": env.int('DB_PORT'),
     }
 }
 
---
+++ users/models.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin # Импорт нужных модулей
+# AbstractBaseUser - базовый класс, который служит фундаментом для создания
+# кастомной модели пользователя, имеет нужный нам функционал (аунтефикация, хэширование паролей, сброс)
+# BaseUserManager - базовый класс для менеджеров, служит кастомной логикой для создания пользователя
+# PermissionsMixin - абстрактный базовый класс, который упрощает интеграцию системы разрешений
+# фреймворка в кастомную модель (тут как раз появляется is_superuser и is_staff)
+from django.db import models
+
+# Create your models here.
+class UserManager(BaseUserManager): # Клас создания пользователя и админа
+  def create_user(self, email, password, **exstra_fields): # Функция создания пользователя
+    if not email: # Если поле пустое, то появляется ошибка
+      raise ValueError('Email is required') # raise ValueError - создание исключения, который прерывает текущую функцию,
+                                            # текст ошибки
+    email = self.normalize_email(email) # normalize_email - нормализация почты, все, что после @ получается нижним регистром
+    user = self.model(email=email, **exstra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_superuser(self, email, password, **exstra_fields): # Функция создания суперпользователя
+    exstra_fields['is_staff'] = True
+    exstra_fields['is_superuser'] = True
+
+    return self.create_user(email, password, **exstra_fields)
+
+class Role(models.TextChoices): # Создание класса с полями для выбора роли
+    USER= 'user', 'Пользователь'
+    SUPPORT = 'support', 'Саппорт'
+    MANAGER = 'manager', 'Менеджер'
+    ADMIN = 'admin', 'Администратор'
+
+class User(AbstractBaseUser, PermissionsMixin): # Класс пользователя с полями
+  first_name = models.CharField(max_length=50) # CharFiled - текстовое поле для небольшого объема,
+                                                           # max_length - максимальная длина, null - пустое или нет
+  last_name = models.CharField(max_length=50)
+  email = models.EmailField(unique=True, null=False, blank=False) # EmailField - поле почты,
+                                                                  # unique - уникальное/не уникальное,
+                                                                  # blank=False - обязательное/не обязательное
+  phone_number = models.CharField(max_length=50, blank=False, unique=True) # Правильная запись будет через валидатор в форме
+  is_active = models.BooleanField(default=True) # is_active - активен или деактивирован ли аккаунт, BooleanField -
+                                                # булево значение (True/False), default - что будет по умолчанию
+  is_staff = models.BooleanField(default=False) # is_staff - флаг, который определяет доступ к админке
+  photo = models.ImageField(upload_to='photo/', # ImageField - поле для изображения, хранит путь, upload_to - параметры куда загружать
+                            blank=True,
+                            null=True,
+                            verbose_name='Фото') # verbose_name - удобное название для интерфейса
+  role = models.CharField(max_length=15, choices=Role.choices, default=Role.USER)
+  created_at = models.DateTimeField(auto_now_add=True) # DateTimeField - поле времени, auto_now_add=True - текущее время при создании
+
+  objects = UserManager()
+
+  USERNAME_FIELD = 'email' # атрибут в модели, который указывает, что будет уникальным идентификатором для аунтификации
+  REQUIRED_FIELDS = ['first_name', 'last_name'] # атрибут, который указывает, какие поля будут запрашиваться для создания суперпользователя
+
+  def __str__(self):
+    return self.email # удобное отображение в консоли/админке
+
