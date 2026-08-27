d1099646404a7df61927e156c61f810de77a5580
Михаил <mnovikov920@gmail.com>
2026-08-27 18:45:33 +0300

    Add login and logout

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import (
     UserCreationForm as BaseUserCreationForm,
     UserChangeForm as BaseUserChangeForm,
 )
+from django import forms
 from .models import User
 
 class UserCreationForm(BaseUserCreationForm):
@@ -27,3 +28,7 @@ class UserChangeForm(BaseUserChangeForm):
       'photo',
       'role'
     )
+
+class LoginForm(forms.Form):
+  email = forms.EmailField()
+  password = forms.CharField(widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from .forms import LoginForm
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+      form = LoginForm(request.POST)
+      if form.is_valid():
+         email = form.cleaned_data['email']
+         password = form.cleaned_data['password']
+         user = authenticate(request, email=email, password=password)
+         if user is not None:
+            login(request, user)
+            return redirect('ticket')
+    else:
+      form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+  logout(request)
+  return redirect('login')
