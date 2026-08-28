58bde8513d54529fdee3f6fb5ed64c49e72bef76
Михаил <mnovikov920@gmail.com>
2026-08-28 18:07:43 +0300

    Configure roles and permissions

--- SupportFlow/settings.py
+++ SupportFlow/settings.py
@@ -115,9 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = [
+  ('ru', 'Русский'),
+  ('en', 'English'),
+]
+
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
